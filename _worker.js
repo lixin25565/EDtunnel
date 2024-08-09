2f95d10ac79652d5c653d8872e1a95113de0dc5c
lixin25565 <102230050+lixin25565@users.noreply.github.com>
2024-08-09 16:07:11 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'e564ea51-d060-4df7-8c1a-c4f7579d4e9a';
 
-const พร็อกซีไอพีs = ['146.70.175.104'];
+const พร็อกซีไอพีs = ['146.70.175.111'];
 // ips：146.70.175.116；146.70.175.112；146.70.175.101；146.70.175.111；146.70.175.104
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
